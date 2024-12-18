9664dece032f40f7904a7f825c2229f26b971ab8
mingyue0117 <3103334911@qq.com>
2024-12-18 21:44:17 +0800

    增加logs页面 显示登录数据

--- src/main/java/cn/edu/ujn/Forum/dao/LogsMapper.java
+++ src/main/java/cn/edu/ujn/Forum/dao/LogsMapper.java
@@ -2,8 +2,12 @@ package cn.edu.ujn.Forum.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface LogsMapper {
     // 插入一条登录记录
     void insertLog(Logs logs);
+    List<Logs> selectLogsByUserId(Integer userId);
+
 }
